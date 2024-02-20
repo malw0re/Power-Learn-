69da751fdd945bc084c95792100f2a302dda6cb8
Malw0re <malw.karanja@gmail.com>
2024-02-20 21:19:12 +0300

    python files added

---
+++ python/user_input.py
@@ -0,0 +1,7 @@
+#!/bin/python3
+name = input("Enter your name? ")
+age = input("Enter your age? ")
+location = input("Enter your geographical location ")
+
+
+print(f"Hello {name}, you are {age} years old and live in {location}")
